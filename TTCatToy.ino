7137f7d8b487f43b48965252efa61f6b413a4262
mploof <michael@tinkertech.io>
2019-06-13 16:57:11 -0400

    Added comments, removed some unused variables

--- TTCatToy.ino
+++ TTCatToy.ino
@@ -1,44 +1,65 @@
-#include <Adafruit_NeoPixel.h>
-#include <Adafruit_TiCoServo.h>
-#include <known_16bit_timers.h>
+/*
+ *  Typically, the "Servo.h" library would be used, but
+ *  because servos and programmable LEDs both rely on interrupts,
+ *  the "Adafruit_TiCoServo.h" is used instead. This library
+ *  generates PWM pulses for servo control via hardware peripherals
+ *  instead of timer interrupts. If the normal "Servo.h" library
+ *  is used, the servos jitter uncontrollably and the LEDs
+ *  generally don't respond as expected.
+ */
+#include <Adafruit_NeoPixel.h>    // RGB LED Library
+#include <Adafruit_TiCoServo.h>   // Servo library
+#include <known_16bit_timers.h>   // Servo library dependency
 
-#define LED_PIN     4
-#define LED_COUNT  16
-#define BRIGHTNESS 20
+// Constants for LEDs
+#define RGB_LED_DATA_PIN  4  // Arduino data pin
+#define LED_COUNT         16 // Total number of LEDs
+#define BRIGHTNESS        20 // Starting overall brightness
 
+// Possible state machine states
 typedef enum _mode {
   MODE_NORMAL = 0,
   MODE_AUTO,
   MODE_LED
 } MODE;
 
+// Arduino pin connections
 const int BUTTON = 12;
 const int LED = 6;
 const int PAN_POT = A0;
 const int TILT_POT = A1;
 const int LASER = 5;
 
+// Servo range limits in degrees
+// These prevent the servos from moving to useless physical positions
 const int MIN_PAN = 23;
 const int MAX_PAN = 150;
 const int MIN_TILT = 2;
 const int MAX_TILT = 37;
 
+// Servo objects
 Adafruit_TiCoServo pan;
 Adafruit_TiCoServo tilt;
 
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-
+// Current servo position vars
 int tilt_pos = 0;
 int pan_pos = 0;
 
+// RGB LED strand object
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+// State machine state
 MODE run_mode = MODE_NORMAL;
 
-int led_brightness = 0;
-int led_increment = 5;
-int laser_brightness = 30;
+// Initial laser PWM value
+int laser_brightness = 255;
 
-bool position_debug = true;
+// Debug var for toggling serial printing
+const bool POSITION_DEBUG = true;
 
+/*
+ *  Initial configuration
+ */
 void setup() {
   // Setup serial
   Serial.begin(115200);
@@ -55,19 +76,25 @@ void setup() {
   // Setup other peripherals
   pinMode(LASER, OUTPUT);
   pinMode(LED, OUTPUT);
+  // Use internal pullup resistor to avoid floating pin when button is not pressed
   pinMode(BUTTON, INPUT_PULLUP);
   digitalWrite(LED, LOW);
   analogWrite(LASER, laser_brightness);
 
-  // Seed the random number generator
+  // Seed the random number generator with noise on an unused analog pin
   randomSeed(analogRead(3));
 
 }
 
+/*
+ *  Main loop
+ */
 void loop() {
 
+  // See if we need to switch states
   checkButton();
 
+  // Run the current state
   switch (run_mode) {
     case MODE_NORMAL:
       runNormalMode();
@@ -82,25 +109,35 @@ void loop() {
 
 }
 
+/*
+ *  Checks to see if button is pressed and handles
+ *  state machine transitions.
+ */
 void checkButton() {
   static bool button_was_pressed = false;
   static long last_button_check = millis();
   const int BUTTON_CHECK_INTERVAL = 100;
 
+  // Only check once in a while to prevent reading phyical button "bounce"
   if (millis() - last_button_check > BUTTON_CHECK_INTERVAL) {
 
+    // Because the default (unpressed) state on the button pin is high, 
+    // due to the pull-up resistor, flip the logic
     bool button_pressed = !digitalRead(BUTTON);
 
+    // While the button is held down...
     while (button_pressed && button_was_pressed) {
       int laser_brightness = map(analogRead(TILT_POT), 0, 1023, 0, 255);
       analogWrite(LASER, laser_brightness);
       button_pressed = !digitalRead(BUTTON);
     }
 
+    // When the button is released
     if (!button_pressed && button_was_pressed) {
       Serial.println("Button pressed!");
       Serial.println(run_mode);
 
+      // Transition to the next state
       switch (run_mode) {
         case MODE_NORMAL:
           {
@@ -119,6 +156,8 @@ void checkButton() {
 
             digitalWrite(LED, LOW);
 
+            // Convert the current potentiometer positions into a color
+            // and brightness for the programmable RGB LEDs and set them
             int pan_raw = analogRead(PAN_POT);
             int tilt_raw = analogRead(TILT_POT);
             int fade_val = map(pan_raw, 0, 1023, 60, 255);
@@ -133,7 +172,7 @@ void checkButton() {
           {
             Serial.println("Entering normal mode");
 
-            colorWipe(strip.Color(  0,   0, 0), 40); // Off
+            colorWipe(strip.Color(  0,   0, 0), 40); // Clear the programmable RGB LEDs
 
             run_mode = MODE_NORMAL;
             break;
@@ -141,21 +180,26 @@ void checkButton() {
 
         default:
           {
+            // We shouldn't ever end up here, but just in case...
             Serial.println("Invalid case");
             break;
           }
       }
     }
+
+    // Save the current time to check against when computing the next interval
     last_button_check = millis();
+    // Save the current button state to compare against next time
     button_was_pressed = button_pressed;
   }
 }
 
 
+// Noraml mode state
 void runNormalMode() {
   int pan_raw = analogRead(PAN_POT);
   int tilt_raw = analogRead(TILT_POT);
-  if (position_debug) {
+  if (POSITION_DEBUG) {
     Serial.print(pan_raw);
     Serial.print(", ");
     Serial.println(tilt_raw);
@@ -166,6 +210,8 @@ void runNormalMode() {
   delay(50);
 }
 
+
+// Auto Mode State
 void runAutoMode() {
 
   const int MIN_RAPID_INTERVAL = 100;
@@ -233,6 +279,8 @@ void runAutoMode() {
   }
 }
 
+
+// LED Mode State
 void runLEDMode() {
 
   int pan_raw = analogRead(PAN_POT);
@@ -252,30 +300,34 @@ void runLEDMode() {
   delay(10);
 }
 
+
 void updateServos() {
   static int old_pan_pos = 0;
   static int old_tilt_pos = 0;
 
+  // Requiring a change of greater than 1 cuts down on jitter
   if (abs(old_pan_pos - pan_pos) > 1) {
     pan.write(pan_pos);
-    if (position_debug) {
+    if (POSITION_DEBUG) {
       Serial.print("Pan: ");
       Serial.print(pan_pos);
       Serial.print(" ");
     }
   }
 
+  // Requiring a change of greater than 1 cuts down on jitter
   if (abs(old_tilt_pos - tilt_pos) > 1) {
     tilt.write(tilt_pos);
-    if (position_debug) {
+    if (POSITION_DEBUG) {
       Serial.print("Tilt: ");
       Serial.print(tilt_pos);
     }
   }
-  if (position_debug) {
+  if (POSITION_DEBUG) {
     Serial.println("\n");
   }
 }
+
 
 void colorWipe(uint32_t color, int wait) {
   for (int i = 0; i < strip.numPixels(); i++) { // For each pixel in strip...
